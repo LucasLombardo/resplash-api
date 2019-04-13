521f9f19c8c3bbfc6e7e4859ba215dc3abaddd39
LucasLombardo <lucasalombardo@gmail.com>
2019-04-12 20:29:39 -0400

    add createPhoto mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutation = {};
+const Mutation = {
+
+    async createPhoto(parent, args, ctx, info) {
+        const photo = await ctx.db.mutation.createPhoto({
+            data: { ...args.data }
+        }, info);
+        return photo;
+    } 
+  
+};
 
 module.exports = Mutation;
