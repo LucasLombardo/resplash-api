2985a7212f89603bad05b00806c878385e73ee1b
LucasLombardo <lucasalombardo@gmail.com>
2019-04-12 20:42:20 -0400

    add query for photos

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const { forwardTo } = require(`prisma-binding`);
+
+const Query = {
+
+    // forward to prisma since no auth needed
+    photos: forwardTo(`db`)
+
+};
 
 module.exports = Query;
